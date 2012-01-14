e8b97f826ef4fca81c568c3675672e3d81e0a0fb
Thomas Lafarge <thomas.lafarge@nist.gov>
2012-01-14 09:00:20 +0000

    version 1.0

--- R/rtorus.R
+++ R/rtorus.R
@@ -1,12 +1,18 @@
 rtorus <-
-function (n, r, R, ct = c(0, 0, 0)) 
+function (n, r, R, ct = c(0, 0, 0), rotx = NULL) 
 {
     s <- runif(n, 0, 2 * pi)
     t <- runif(n, 0, 2 * pi)
     Rg <- runif(n, r, R)
     rg <- runif(n, 0, r)
-    x <- cos(s) * (R + rg * cos(t))
-    y <- sin(s) * (R + rg * cos(t))
-    z <- r * sin(t)
-    return(cbind(ct[1] + x, ct[2] + y, ct[3] + z))
+    x <- ct[1] + cos(s) * (R + rg * cos(t))
+    y <- ct[2] + sin(s) * (R + rg * cos(t))
+    z <- ct[3] + r * sin(t)
+    if (!is.null(rotx)) {
+        aux1 <- cos(rotx) * y - sin(rotx) * z
+        aux2 <- sin(rotx) * y + cos(rotx) * z
+        y <- aux1
+        z <- aux2
+    }
+    return(cbind(x, y, z))
 }
