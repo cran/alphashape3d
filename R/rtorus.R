0eecfc83bffbf895b2896cdbc160d8fd366dabe5
Beatriz Pateiro-Lopez <beatriz.pateiro@usc.es>
2013-06-05 13:55:09 +0000

    version 1.0-3

--- R/rtorus.R
+++ R/rtorus.R
@@ -1,18 +1,36 @@
 rtorus <-
 function (n, r, R, ct = c(0, 0, 0), rotx = NULL) 
 {
-    s <- runif(n, 0, 2 * pi)
-    t <- runif(n, 0, 2 * pi)
-    Rg <- runif(n, r, R)
-    rg <- runif(n, 0, r)
-    x <- ct[1] + cos(s) * (R + rg * cos(t))
-    y <- ct[2] + sin(s) * (R + rg * cos(t))
-    z <- ct[3] + r * sin(t)
-    if (!is.null(rotx)) {
-        aux1 <- cos(rotx) * y - sin(rotx) * z
-        aux2 <- sin(rotx) * y + cos(rotx) * z
-        y <- aux1
-        z <- aux2
+    if (r < 0 | R < 0 | r > R) {
+        stop("The input parameters must satisfy 0 <= r <= R")
     }
-    return(cbind(x, y, z))
+    norig <- n
+    dif <- R - r
+    rsq <- r^2
+    sumRr <- r + R
+    x <- rep(0, 3)
+    while (n > 0) {
+        U <- runif(n)
+        Rg <- sqrt(U * sumRr^2 + (1 - U) * dif^2)
+        Z <- r * (2 * runif(n) - 1)
+        phi <- 2 * pi * runif(n)
+        aux <- cbind(Rg * cos(phi), Rg * sin(phi), Z)
+        pin <- (Rg - R)^2 + Z^2 <= rsq
+        if (sum(pin) > 0) {
+            x <- rbind(x, aux[pin, ])
+        }
+        n <- ifelse(sum(pin) < n, n - sum(pin), 0)
+    }
+    x <- x[-1, ]
+    x <- x[1:norig, ]
+    x[, 1] <- ct[1] + x[, 1]
+    x[, 2] <- ct[2] + x[, 2]
+    x[, 3] <- ct[3] + x[, 3]
+    if (!is.null(rotx)) {
+        aux1 <- cos(rotx) * x[, 2] - sin(rotx) * x[, 3]
+        aux2 <- sin(rotx) * x[, 2] + cos(rotx) * x[, 3]
+        x[, 2] <- aux1
+        x[, 3] <- aux2
+    }
+    return(x)
 }
